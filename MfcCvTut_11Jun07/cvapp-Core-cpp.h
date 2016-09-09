4261410ae2141397e061509fa466dea9eda2fdda
haytham magdi <haytham.magdi@hotmail.com>
2016-09-09 16:21:33 +0300

    done replacememt of ImgRotationMgr with ImgRotationMgr_2

--- MfcCvTut_11Jun07/cvapp-Core-cpp.h
+++ MfcCvTut_11Jun07/cvapp-Core-cpp.h
@@ -2916,10 +2916,10 @@ void ImageProcessor::Try26()
 	F32ImageRef src = GlobalStuff::GetLinePathImg();
 
 
-	//ImgRotationMgr_2_Ref rotMgr88_0_0 = new ImgRotationMgr_2(src, 0);
-	//ImgRotationMgr_2_Ref rotMgr88_0_1 = new ImgRotationMgr_2(src, 22.5);
-	//ImgRotationMgr_2_Ref rotMgr88_0_2 = new ImgRotationMgr_2(src, 45);
-	//ImgRotationMgr_2_Ref rotMgr88_0_3 = new ImgRotationMgr_2(src, 67.5);
+	//ImgRotationMgrRef rotMgr88_0_0 = new ImgRotationMgr(src, 0);
+	//ImgRotationMgrRef rotMgr88_0_1 = new ImgRotationMgr(src, 22.5);
+	//ImgRotationMgrRef rotMgr88_0_2 = new ImgRotationMgr(src, 45);
+	//ImgRotationMgrRef rotMgr88_0_3 = new ImgRotationMgr(src, 67.5);
 
 	////GlobalStuff::SetLinePathImg(rotMgr88_0_1->GetResImg());
 	//GlobalStuff::SetLinePathImg(rotMgr88_0_2->GetResImg());
@@ -2927,12 +2927,12 @@ void ImageProcessor::Try26()
 
 	//return;
 
-	//ImgRotationMgr_2_Ref rotMgr88 = new ImgRotationMgr_2(src, 45);
-	//ImgRotationMgr_2_Ref rotMgr88 = new ImgRotationMgr_2(src, 55);
-	ImgRotationMgr_2_Ref rotMgr88 = new ImgRotationMgr_2(src, 0);
-	//ImgRotationMgr_2_Ref rotMgr88 = new ImgRotationMgr_2(src, 70);
-	//ImgRotationMgr_2_Ref rotMgr88 = new ImgRotationMgr_2(src, 89);
-	//ImgRotationMgr_2_Ref rotMgr88 = new ImgRotationMgr_2(src, 20);
+	//ImgRotationMgrRef rotMgr88 = new ImgRotationMgr(src, 45);
+	//ImgRotationMgrRef rotMgr88 = new ImgRotationMgr(src, 55);
+	ImgRotationMgrRef rotMgr88 = new ImgRotationMgr(src, 0);
+	//ImgRotationMgrRef rotMgr88 = new ImgRotationMgr(src, 70);
+	//ImgRotationMgrRef rotMgr88 = new ImgRotationMgr(src, 89);
+	//ImgRotationMgrRef rotMgr88 = new ImgRotationMgr(src, 20);
 	//F32ImageRef res1 = rotMgr1->GetResImg();
 
 	src = rotMgr88->GetResImg();
@@ -3068,7 +3068,7 @@ void ImageProcessor::Try26()
 	Ns_Saica::RotationMgrCollRef rotColl1 = new  Ns_Saica::RotationMgrColl(src, 4);
 	//Ns_Saica::RotationMgrCollRef rotColl1 = new  Ns_Saica::RotationMgrColl(src, 8);
 
-	ImgRotationMgr_2_Ref rot33 = rotColl1->GetRotAt(0);
+	ImgRotationMgrRef rot33 = rotColl1->GetRotAt(0);
 	ImageItrMgrRef imgItr33 = rot33->GetImageItrMgr();
 	ImageLineItrProvider & imgLineItrPorv33 = imgItr33->GetItrProvAt(1);
 	FixedVector< ImageLineItr > & lineItrArr33 = imgLineItrPorv33.GetLineItrArr();
@@ -3091,8 +3091,8 @@ void ImageProcessor::Try26()
 	//ShowImage(rotColl1->GetRotAt(6)->GetResImg(), "Rot1_6");
 	//ShowImage(rotColl1->GetRotAt(7)->GetResImg(), "Rot1_7");
 
-	//ImgRotationMgr_2_Ref rotMgr1 = new ImgRotationMgr(src, 45);
-	ImgRotationMgr_2_Ref rotMgr1 = rotColl1->GetRotAt(1);
+	//ImgRotationMgrRef rotMgr1 = new ImgRotationMgr(src, 45);
+	ImgRotationMgrRef rotMgr1 = rotColl1->GetRotAt(1);
 
 	F32ImageRef res1 = rotMgr1->GetResImg();
 
